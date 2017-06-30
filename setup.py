e4161850a4679ede9ce381749569075c738d8387
Timon Schmelzer <timon.schmelzer@tu-dortmund.de>
2017-06-30 18:46:35 +0200

    Fix including bug

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'mygreeter',
     packages = ['mygreeter'],
-    version = 'v0.0.1',  # Ideally should be same as your GitHub release tag varsion
+    version = 'v0.0.2',  # Ideally should be same as your GitHub release tag varsion
     description = 'Package to greet me, you or the world',
     author = 'Timon Schmelzer',
     author_email = 'timon.schmelzer@tu-dortmund.de',
     url = 'https://github.com/Kecksdose/mygreeter',
-    download_url = 'https://github.com/Kecksdose/mygreeter/archive/v0.0.1.tar.gz',
+    download_url = 'https://github.com/Kecksdose/mygreeter/archive/v0.0.2.tar.gz',
     keywords = ['hello world', 'greeter', 'greetings'],
     classifiers = [],
 )
