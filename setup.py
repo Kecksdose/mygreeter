ed2df2c87bba4efc3e21e7f8da8d44cab62e3b90
Timon Schmelzer <timon.schmelzer@tu-dortmund.de>
2017-06-30 18:44:33 +0200

    Ready for production

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'mygreeter',
+    packages = ['mygreeter'],
+    version = 'v0.0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Package to greet me, you or the world',
+    author = 'Timon Schmelzer',
+    author_email = 'timon.schmelzer@tu-dortmund.de',
+    url = 'https://github.com/Kecksdose/mygreeter',
+    download_url = 'https://github.com/Kecksdose/mygreeter/archive/v0.0.1.tar.gz',
+    keywords = ['hello world', 'greeter', 'greetings'],
+    classifiers = [],
+)
